85f6e4d961030079bb97310891c9ddc5ea0c3b49
Scherbatykh Denis <DScherbatykh@sportmaster.ru>
2024-07-14 16:45:23 +0300

    project create

---
+++ src/test/java/com/shdv09/pro_fit/ProFitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shdv09.pro_fit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProFitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
