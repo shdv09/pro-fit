a2e7679589f32e5f66f0c8ebd58b6d37abde7c1d
Denis Scherbatykh <shdv09@gmail.com>
2025-08-08 16:06:53 +0300

    fix code style

--- appointment-service/src/main/java/com/shdv09/appointmentservice/service/DateFactory.java
+++ appointment-service/src/main/java/com/shdv09/appointmentservice/service/DateFactory.java
@@ -3,7 +3,6 @@ package com.shdv09.appointmentservice.service;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 
 @Component
 public class DateFactory {
